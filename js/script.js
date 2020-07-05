78169087ff31c7da587ece239c85797b060eaa0e
TimaPaps <ptv.devstudio@gmail.com>
2020-07-06 02:04:13 +0500

    добавил footer, добавил кнопку закрытия выползающего меню и событие закрытия меню

--- js/script.js
+++ js/script.js
@@ -1,17 +1,42 @@
 //выползающее меню header
 let countMenuBtn = 0;
 
-$('.header-menu-btn').click(function() {
+//if ($(window).width() < 700) {
+      
+    $('.header-menu-btn').click(function() {
 
-    if (countMenuBtn == 0) {
-        countMenuBtn++;
-        $('.header-nav').animate({
-            'left': 0
-        })
-    } else {
-        countMenuBtn--;
-        $('.header-nav').animate({
-            'left': -100 + '%'
-        })
-    }    
-});
+   
+        if (countMenuBtn == 0) {
+            countMenuBtn++;
+            $('.top-slider-right-btn').css('z-index', 0);
+            $('.top-slider-left-btn').css('z-index', 0);
+            $('.bottom-slider-right-btn').css('z-index', 0);
+            $('.bottom-slider-left-btn').css('z-index', 0);
+            $('.header-nav').animate({
+                'left': 0
+            });   
+            $('.header-menu-btn').css('display', 'none');
+            $('.header-menu-close-btn').css('display', 'block');
+        }    
+    });
+    
+    $('.header-menu-close-btn').click(function() {
+    
+        if (countMenuBtn != 0) {
+            countMenuBtn--;
+            $('.top-slider-right-btn').css('z-index', 2);
+            $('.top-slider-left-btn').css('z-index', 2);
+            $('.bottom-slider-right-btn').css('z-index', 2);
+            $('.bottom-slider-left-btn').css('z-index', 2);
+            $('.header-nav').animate({
+                'left': -100 + '%'
+            });
+            $('.header-menu-btn').css('display', 'block');
+            $('.header-menu-close-btn').css('display', 'none');
+        }
+    });
+
+/*} else {
+    $('.header-menu-btn').css('display', 'none');
+    $('.header-menu-close-btn').css('display', 'none');
+}*/
