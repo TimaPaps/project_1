fc46031f53640a956ab231c28592aa988381b565
TimaPaps <ptv.devstudio@gmail.com>
2020-07-25 20:41:40 +0500

    устранил ошибку в файле register.php - ; - куда то пропала 8-), добавил проверку на заполнение полей формы

--- handlers/register.php
+++ handlers/register.php
@@ -4,68 +4,97 @@
 
 
     //проверка данных формы
+    if (!empty($_POST['userName']) &&   //проверка заполненности полей ФИО, Телефон, E-mail, Ваше сообщение
+        !empty($_POST['email']) &&
+        !empty($_POST['phone']) &&
+        !empty($_POST['textMessage'])) {
+
+        if (mb_strlen($_POST['userName']) <101 &&   //ставлю ограничение на длину ввода в поля ФИО, Телефон и email
+            mb_strlen($_POST['phone']) < 21 && 
+            mb_strlen($_POST['email']) < 101 && 
+            mb_strlen($_POST['textMessage']) < 1001) { 
 
 //  2.1 добавляю данные в БД
-    //создаем запрос в БД
-//    $query = "INSERT INTO `orders` (`userName`, `phone`, `email`, `textMessage`, `created_at`) /*если в БД нет по умолчанию current_timestamp() для поля времени(created_at в моем случае), то сами это сделаем*/
-//              VALUES ('{$_POST['userName']}', '{$_POST['phone']}', '{$_POST['email']}', '{$_POST['textMessage']}', NOW());"; /*прописываем для вставки времени CURRENT_TIMESTAMP или NOW()*/
-    //отправляем запрос в БД, а ответ помещаем в переменную
-//    $result = mysqli_query($link, $query);
+            //создаем запрос в БД
+            //    $query = "INSERT INTO `orders` (`userName`, `phone`, `email`, `textMessage`, `created_at`) /*если в БД нет по умолчанию current_timestamp() для поля времени(created_at в моем случае), то сами это сделаем*/
+            //              VALUES ('{$_POST['userName']}', '{$_POST['phone']}', '{$_POST['email']}', '{$_POST['textMessage']}', NOW());"; /*прописываем для вставки времени CURRENT_TIMESTAMP или NOW()*/
+            //отправляем запрос в БД, а ответ помещаем в переменную
+            //    $result = mysqli_query($link, $query);
 
 //  2.2 добавляю данные в БД в разные таблицы
 
-    //проверка на наличие пользователя в БД
-    $result = mysqli_query($link, "SELECT COUNT(*) as count, `id` FROM `users` WHERE `user_name` = '{$_POST['userName']}' OR `user_phone` = '{$_POST['phone']}' OR `user_email` = '{$_POST['email']}'");
-    $row = mysqli_fetch_assoc($result);
-    //print_r($row);
+            //проверка на наличие пользователя в БД
+            $result = mysqli_query($link, "SELECT COUNT(*) as count, `id` FROM `users` WHERE `user_name` = '{$_POST['userName']}' OR `user_phone` = '{$_POST['phone']}' OR `user_email` = '{$_POST['email']}'");
+            $row = mysqli_fetch_assoc($result);
+            //print_r($row);
 
-    if ($row['count'] < 1) {
-        //добавляю строку в таблицу users
-        $query  = "
-            INSERT INTO `users` (`user_name`, `user_phone`, `user_email`) 
-            VALUES ('{$_POST['userName']}', '{$_POST['phone']}', '{$_POST['email']}');
-        ";
-        //отправляем запрос в БД, а ответ помещаем в переменную
-        $result = mysqli_query($link, $query);
-        // получаю id пользователя при последнем соединении с БД
-        $userID = mysqli_insert_id($link);
-        //echo $userID;
+            if ($row['count'] < 1) {
+                //добавляю строку в таблицу users
+                $query  = "
+                    INSERT INTO `users` (`user_name`, `user_phone`, `user_email`) 
+                    VALUES ('{$_POST['userName']}', '{$_POST['phone']}', '{$_POST['email']}');
+                ";
+                //отправляем запрос в БД, а ответ помещаем в переменную
+                $result = mysqli_query($link, $query);
+                // получаю id пользователя при последнем соединении с БД
+                $userID = mysqli_insert_id($link);
+                //echo $userID;
 
-        //добавляю строку в таблицу questions
-        $query = "
-        INSERT INTO `questions` (`user_id`, `question`) 
-        VALUES ('$userID', '{$_POST['textMessage']}');
-        ";
-        //отправляем запрос в БД, а ответ помещаем в переменную
-        $resultInsertQuestion = mysqli_query($link, $query);
+                //добавляю строку в таблицу questions
+                $query = "
+                INSERT INTO `questions` (`user_id`, `question`) 
+                VALUES ('$userID', '{$_POST['textMessage']}');
+                ";
+                //отправляем запрос в БД, а ответ помещаем в переменную
+                $resultInsertQuestion = mysqli_query($link, $query);
 
-        //и вывожу пользователю сообщение
-        if ($result && $resultInsertQuestion) {
-        $info = 'Отлично! Ваш вопрос отправлен!';
+                //и вывожу пользователю сообщение
+                if ($result && $resultInsertQuestion) {
+                $info = 'Отлично! Ваш вопрос отправлен!';
+                } else {
+                $info = 'Что-то пошло не так. Попробуй еще раз!';
+                }
+
+            } else {
+                // получаю id пользователя из БД из массива $row
+                $userID = $row['id'];
+                //print_r($userID);
+                //добавляю строку в таблицу questions
+                $query = "
+                    INSERT INTO `questions` (`user_id`, `question`) 
+                    VALUES ('$userID', '{$_POST['textMessage']}');
+                ";
+                
+                //отправляем запрос в БД, а ответ помещаем в переменную
+                $resultInsertQuestion = mysqli_query($link, $query);
+
+                //и вывожу пользователю сообщение
+                if ($resultInsertQuestion) {
+                    $info = 'Отлично! Ваш вопрос отправлен!';
+                } else {
+                    $info = 'Что-то пошло не так. Попробуй еще раз!';
+                }
+            }
+
+            //вывод сообщения пользователю    
+            $infoForUser = "
+                Уважаемый(ая), {$_POST['userName']} <br>
+                Мы свяжемся с Вами в ближайшее время!
+            ";     
         } else {
-        $info = 'Что-то пошло не так. Попробуй еще раз!';
+            $info = 'Слишком длинное ФИО (>100 символов) <br>
+                    или телефон (>20 цифр) <br>
+                    или email (>100 символов) <br>
+                    или сообщение (>1000 символов)';
+            $infoForUser = 'Пожалуйста заполните правильно все поля!';      
         }
 
     } else {
-        // получаю id пользователя из БД из массива $row
-        $userID = $row['id']
-        //print_r($userID);
-        //добавляю строку в таблицу questions
-        $query = "
-            INSERT INTO `questions` (`user_id`, `question`) 
-            VALUES ('$userID', '{$_POST['textMessage']}');
-        ";
-        
-        //отправляем запрос в БД, а ответ помещаем в переменную
-        $resultInsertQuestion = mysqli_query($link, $query);
-
-        //и вывожу пользователю сообщение
-        if ($resultInsertQuestion) {
-            $info = 'Отлично! Ваш вопрос отправлен!';
-        } else {
-            $info = 'Что-то пошло не так. Попробуй еще раз!';
-        }
+        $info = 'Поля ФИО, Телефон, E-mail и Ваше сообщение - обязательны для заполнения.';
+        $infoForUser = 'Пожалуйста заполните все поля!';
     }
+
+
     
     //добавляю строку в таблицу users
 //    $query  = "
@@ -109,7 +138,8 @@
     <main>
         <div class="wrapper-block">
             <?php
-                echo "$info";
+                echo "$info <br>";
+                echo "$infoForUser";
             ?>            
         </div>
     </main>
